39b0fda2cf1c5d0fb667b83f766f37b4d902527a
Mayel <git@mayel.space>
2021-03-16 22:08:36 +0100

    clean up queries

--- lib/boundaries/queries.ex
+++ lib/boundaries/queries.ex
@@ -18,34 +18,39 @@ defmodule Bonfire.Boundaries.Queries do
   alias Bonfire.Me.Users
   alias Bonfire.Boundaries.Circles
 
-  @doc "The `add_perms(bool?, bool?)` postgres function for combining permissions."
-  defmacro add_perms(l, r) do
-    quote do: Ecto.Query.fragment("add_perms(?,?)", unquote(l), unquote(r))
-  end
+  @doc """
+  A subquery to join to which filters out results the current user is
+  not permitted to see.
 
-  @doc "The `agg_perms(bool?)` postgres aggregate for combining permissions."
-  defmacro agg_perms(p) do
-    quote do: Ecto.Query.fragment("agg_perms(?)", unquote(p))
-  end
+  Parameters are the alias for the controlled item in the parent
+  query and an expression evaluating to the current user.
+
+  Currently requires a lateral join, but this shouldn't be necessary
+  once we've figured out how to make ecto support what we do better.
+  """
+  defmacro can_see?(controlled, user), do: can(controlled, user, :see)
+
+  defmacro can_read?(controlled, user), do: can(controlled, user, :read)
+
+  defmacro can_edit?(controlled, user), do: can(controlled, user, :edit)
+
+  defmacro can_delete?(controlled, user), do: can(controlled, user, :delete)
 
   defmacro can?(controlled, user, verb), do: can(controlled, user, verb)
 
   defp can({controlled, _, _}, user, verb), do: can(controlled, user, verb)
   defp can(controlled, user, verb) when is_atom(controlled) do
-    circles = Circles.circles()
-    guest = circles[:guest]
-    local = circles[:local]
     quote do
       require Ecto.Query
       require Bonfire.Boundaries.Queries
       verb = Bonfire.Data.AccessControl.Verbs.id!(unquote(verb))
       case unquote(user) do
-        nil ->
-          unquote(guest_can(controlled))
         %{instance_admin: %{is_instance_admin: true}} ->
           unquote(admin_can(controlled, user))
         %{id: user_id} ->
           unquote(user_can(controlled, user))
+        _ ->
+          unquote(guest_can(controlled))
       end
       |> Ecto.Query.subquery()
       # q = Ecto.Query.from controlled in Bonfire.Data.AccessControl.Controlled,
@@ -68,7 +73,7 @@ defmodule Bonfire.Boundaries.Queries do
   defp shared_can(controlled, before, extra) do
     quote do
       unquote(before)
-      Ecto.Query.from controlled in Bonfire.Data.AccessControl.Controlled,
+      Ecto.Query.from(controlled in Bonfire.Data.AccessControl.Controlled, [
         join: acl in assoc(controlled, :acl),
         join: grant in assoc(acl, :grants),
         join: access in assoc(grant, :access),
@@ -79,7 +84,9 @@ defmodule Bonfire.Boundaries.Queries do
         where: controlled.id == parent_as(unquote(controlled)).id,
         group_by: [controlled.id, interact.id],
         having: fragment("agg_perms(?)", interact.value),
-        select: struct(interact, [:id])
+        select: struct(interact, [:id]),
+        # unquote_splicing(extra)
+      ])
     end
   end
 
@@ -91,13 +98,6 @@ defmodule Bonfire.Boundaries.Queries do
     shared_can(controlled, before, extra)
   end
 
-  defp admin_can(controlled, _user) do
-    extra = quote do: [
-      # TODO
-    ]
-    shared_can(controlled, nil, extra)
-  end
-
   defp user_can(controlled, _user) do
     extra = quote do: [
       left_join: encircle in assoc(circle, :encircles),
@@ -106,25 +106,26 @@ defmodule Bonfire.Boundaries.Queries do
     shared_can(controlled, nil, extra)
   end
 
-  @doc """
-  A subquery to join to which filters out results the current user is
-  not permitted to see.
+  @doc "Checks if an admin OR the user can"
+  defp admin_can(controlled, _user) do
+    before = quote do: admin_circle_id = Bonfire.Boundaries.Circles.circles()[:admin]
+    extra = quote do: [
+      left_join: encircle in assoc(circle, :encircles),
+      where: circle.id == ^admin_circle_id or grant.subject_id == ^user_id or encircle.subject_id == ^user_id,
+    ]
+    shared_can(controlled, before, extra)
+  end
 
-  Parameters are the alias for the controlled item in the parent
-  query and an expression evaluating to the current user.
 
-  Currently requires a lateral join, but this shouldn't be necessary
-  once we've figured out how to make ecto support what we do better.
+  @doc "Call the `add_perms(bool?, bool?)` postgres function for combining permissions."
+  defmacro add_perms(l, r) do
+    quote do: Ecto.Query.fragment("add_perms(?,?)", unquote(l), unquote(r))
+  end
 
-  Does not recognise admins correctly right now, they're treated as regular users.
-  """
-  defmacro can_see?(controlled, user), do: can(controlled, user, :see)
-
-  defmacro can_read?(controlled, user), do: can(controlled, user, :read)
-
-  defmacro can_edit?(controlled, user), do: can(controlled, user, :edit)
-
-  defmacro can_delete?(controlled, user), do: can(controlled, user, :delete)
+  @doc "Call the `agg_perms(bool?)` postgres aggregate for combining permissions."
+  defmacro agg_perms(p) do
+    quote do: Ecto.Query.fragment("agg_perms(?)", unquote(p))
+  end
 
   @doc """
   Lists permitted interactions on something for the current user.
@@ -138,13 +139,13 @@ defmodule Bonfire.Boundaries.Queries do
   Does not recognise admins correctly right now, they're treated as regular users.
   """
   def permitted_on(controlled, user) do
-    local = Users.local_user_id()
+    local = Users.local_user_id() # FIXME, what should this do?
     guest = Users.guest_user_id()
     quote do
       require Ecto.Query
       users = case unquote(user) do
-        nil -> [unquote(guest)]
         %{id: id} -> [id, unquote(local)]
+        _ -> [unquote(guest)]
       end
       Ecto.Query.from controlled inon Bonfire.Data.AccessControl.Controlled,
         join: acl in assoc(controlled, :acl),
