fe4fcd9235e6cc107c2fff2cb23c5e25477420fc
Mayel <git@mayel.space>
2021-05-29 18:56:56 +0200

    fix verb ulids

--- lib/boundaries/verbs.ex
+++ lib/boundaries/verbs.ex
@@ -4,13 +4,16 @@ defmodule Bonfire.Boundaries.Verbs do
   import Bonfire.Boundaries.Integration
   import Ecto.Query
 
-  def verbs do
+  def declare_verbs do
     Bonfire.Common.Config.get!(:verbs)
   end
 
-def get(verb) do
-Bonfire.Common.Config.get!([:verbs, verb])
-end
+  def verbs, do: declare_verbs
+
+  def get(verb) do
+    Bonfire.Common.Config.get!([:verbs, verb])
+    # TODO use Verbs service
+  end
 
   def verbs_fixture do
     Enum.map(verbs(), fn {k, v} -> %{id: v, verb: to_string(k)} end)
@@ -24,6 +27,63 @@ end
     Verb.changeset(verb, attrs)
   end
 
-  def list, do: repo().all(from(u in Verb))
+  def list(from \\ :db)
+  def list(:db) do
+    repo().all(Verb)
+    |> Enum.reduce(%{}, fn t, acc ->
+      Map.merge(acc, %{t.verb => t})
+    end)
+  end
+  def list(:code), do: Bonfire.Data.AccessControl.Verbs.data
+
+
+  def list_verbs_debug() do
+    Enum.concat(list_verbs_db_vs_code(), list_verbs_code_vs_db())
+    |> Enum.sort(:desc)
+    |> Enum.dedup
+  end
+
+  defp list_verbs_db_vs_code() do
+    list(:db)
+    |> Enum.map(fn {verb, t} ->
+      verb = String.to_atom(verb)
+      with {:ok, p} <- Bonfire.Data.AccessControl.Verbs.verb(verb) do
+        if t.id == p.id do
+          {:ok, verb}
+        else
+          {:error, "Code and DB have differing IDs for the same verb", verb, p.id, t.id}
+        end
+      else e ->
+        {:error, "Verb present in DB but not in code", verb}
+      end
+    end)
+    |> Enum.sort(:desc)
+  end
+
+
+  defp list_verbs_code_vs_db() do
+    db_verbs = list(:db)
+
+    list(:code)
+    |> Enum.map(fn {schema, p} when is_atom(schema) ->
+
+      t = Map.get(db_verbs, Atom.to_string(p.verb))
+
+      if not is_nil(t) do
+        if t.id == p.id do
+          {:ok, p.verb}
+        else
+          {:error, "Code and DB have differing IDs for the same verb", p.verb, p.id, t.id}
+        end
+      else
+        {:error, "Verb present in code but not in DB", p.verb}
+      end
+
+      _ -> nil
+    end)
+    |> Enum.sort(:desc)
+    |> Enum.dedup
+  end
+
 
 end
