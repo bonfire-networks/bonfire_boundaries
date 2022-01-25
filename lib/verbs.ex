100c308479b168e325b0f2bfb6eb4161c4180ff9
jjl <james.laver@gmail.com>
2022-01-25 18:40:09 +0100

    fix

--- lib/verbs.ex
+++ lib/verbs.ex
@@ -9,12 +9,16 @@ defmodule Bonfire.Boundaries.Verbs do
   def get(slug) when is_atom(slug), do: verbs()[slug]
   def get(id_or_name) when is_binary(id_or_name), do: get_tuple(id_or_name) |> elem(1)
 
-  def get_tuple(id_or_name) when is_binary(id_or_name), do: verbs() |> Enum.find(fn {_slug, verb} -> verb[:id] == id_or_name or verb[:verb] == id_or_name end)
+  def get_tuple(id_or_name) when is_binary(id_or_name) do
+    Enum.find verbs, fn {_slug, verb} ->
+      verb[:id] == id_or_name or verb[:verb] == id_or_name
+    end
+  end
 
   def get_slug(id), do: get_tuple(id) |> elem(0)
 
-  def get!(slug) when is_atom(slug) do
-    get(slug) || raise RuntimeError, message: "Missing default verb: #{inspect(slug)}"
+  def get!(id_or_name) when is_atom(id_or_name) or is_binary(id_or_name) do
+    get(id_or_name) || raise RuntimeError, message: "Missing default verb: #{inspect(id_or_name)}"
   end
 
   def get_id(slug), do: Map.get(verbs(), slug, %{})[:id]
