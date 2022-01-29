8ff63b21b5c1fe6fc5cae6ae0a16d25d34e63c41
Mayel de Borniol <git@mayel.space>
2022-01-29 17:32:41 +1300

    WIP

--- lib/controlleds.ex
+++ lib/controlleds.ex
@@ -3,6 +3,7 @@ defmodule Bonfire.Boundaries.Controlleds do
   alias Bonfire.Data.AccessControl.Controlled
   import Bonfire.Boundaries
   import Ecto.Query
+  alias Bonfire.Common.Utils
 
   def create(%{}=attrs) when not is_struct(attrs) do
     repo().insert(
@@ -22,5 +23,20 @@ defmodule Bonfire.Boundaries.Controlleds do
     preload: [acl: [:named]]
   ))
 
+  def list_on_object(object), do: list_on_object([object])
+
+  def list_on_objects(objects) do
+    repo().many(list_on_objects_q(objects))
+  end
+
+  def list_on_objects_q(objects, filter_acls \\ [:guests_may_see, :locals_may_interact, :locals_may_reply]) do
+    filter_acls = filter_acls |> Bonfire.Boundaries.Acls.get_id!()
+
+    from c in Controlled,
+    left_join: acl in assoc(c, :acl),
+    left_join: named in assoc(acl, :named),
+    where: c.acl_id in ^filter_acls,
+    where: c.object_id in ^Utils.ulid(objects)
+  end
 
 end
