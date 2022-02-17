7a2dfd0c1b7676fcdfbb39c1a46df32fef1931ed
Mayel de Borniol <git@mayel.space>
2022-02-17 16:30:02 +1300

    clean

--- lib/boundaries.ex
+++ lib/boundaries.ex
@@ -1,10 +1,10 @@
 defmodule Bonfire.Boundaries do
+  use Bonfire.Common.Utils
 
   alias Bonfire.Data.AccessControl.Grant
   alias Bonfire.Data.Identity.Caretaker
   alias Bonfire.Boundaries.Accesses
   alias Bonfire.Boundaries.Circles
-  alias Bonfire.Common.Utils
 
   def repo, do: Bonfire.Common.Config.get!(:repo_module)
   def mailer, do: Bonfire.Common.Config.get!(:mailer_module)
@@ -14,13 +14,11 @@ defmodule Bonfire.Boundaries do
   replacing the existing caretaker, if any.
   """
   def take_care_of!(things, user) when is_list(things) do
-    user_id = Utils.ulid(user)
-    repo().insert_all Caretaker, Enum.map(things, &(%{id: Utils.ulid(&1), caretaker_id: user_id})),
-      on_conflict: :nothing, conflict_target: [:id]
+    repo().insert_all(Caretaker, Enum.map(things, &(%{id: Utils.ulid(&1), caretaker_id: Utils.ulid(user)})), on_conflict: :nothing, conflict_target: [:id]) #|> debug
     Enum.map(things, fn thing ->
       case thing do
         %{caretaker: _} ->
-          Map.put(thing, :caretaker, %Caretaker{id: thing.id, caretaker_id: user.id, caretaker: user})
+          Map.put(thing, :caretaker, %Caretaker{id: thing.id, caretaker_id: Utils.ulid(user), caretaker: user})
         _ -> thing
       end
     end)
