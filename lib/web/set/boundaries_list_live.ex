100a987134d90640c759b9440b84f336966affc8
Mayel de Borniol <git@mayel.space>
2022-08-19 22:59:44 +1200

    props

--- lib/web/set/boundaries_list_live.ex
+++ lib/web/set/boundaries_list_live.ex
@@ -2,11 +2,11 @@ defmodule Bonfire.Boundaries.Web.BoundariesListLive do
   use Bonfire.UI.Common.Web, :stateless_component
   use Bonfire.Common.Utils
 
-  prop create_activity_type, :atom
-  prop to_boundaries, :list
+  prop create_activity_type, :atom, default: nil
+  prop to_boundaries, :list, default: nil
   prop to_boundaries_ids, :list, default: []
-  prop to_circles, :list
-  prop showing_within, :any
+  prop to_circles, :list, default: nil
+  prop showing_within, :any, default: nil
   prop show_select_recipients, :boolean, default: false
   prop open_boundaries, :boolean, default: false
   prop hide_breakdown, :boolean, default: false
