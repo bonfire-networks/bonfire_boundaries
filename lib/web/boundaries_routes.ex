89a989c734593573b498ce3e222f9dbe54bc9199
Mayel de Borniol <git@mayel.space>
2022-10-20 19:55:40 +1300

    declare_routes

--- lib/web/boundaries_routes.ex
+++ lib/web/boundaries_routes.ex
@@ -1,45 +1,47 @@
 defmodule Bonfire.Boundaries.Web.Routes do
+  def declare_routes, do: "boundaries"
+
   defmacro __using__(_) do
     quote do
       # pages anyone can view
-      scope "/", Bonfire.Boundaries.Web do
+      scope "/boundaries", Bonfire.Boundaries.Web do
         pipe_through(:browser)
       end
 
       # pages only guests can view
-      scope "/", Bonfire.Boundaries.Web do
+      scope "/boundaries", Bonfire.Boundaries.Web do
         pipe_through(:browser)
         pipe_through(:guest_only)
       end
 
-      scope "/", Bonfire do
+      scope "/boundaries", Bonfire do
         pipe_through(:browser)
         pipe_through(:account_required)
       end
 
       # pages you need an account to view
-      scope "/", Bonfire.Boundaries.Web do
+      scope "/boundaries", Bonfire.Boundaries.Web do
         pipe_through(:browser)
         pipe_through(:account_required)
       end
 
       # pages you need to view as a user
-      scope "/", Bonfire.Boundaries.Web do
+      scope "/boundaries", Bonfire.Boundaries.Web do
         pipe_through(:browser)
         pipe_through(:user_required)
 
-        live("/boundaries", BoundariesLive)
-        live("/boundaries/:tab", BoundariesLive)
-        live("/boundaries/:tab/:id", BoundariesLive)
-        live("/boundaries/:tab/:id/:section", BoundariesLive, as: :boundaries)
+        live("/", BoundariesLive)
+        live("/:tab", BoundariesLive)
+        live("/:tab/:id", BoundariesLive)
+        live("/:tab/:id/:section", BoundariesLive, as: :boundaries)
       end
 
       # pages only admins can view
-      scope "/", Bonfire.Boundaries.Web do
+      scope "/boundaries", Bonfire.Boundaries.Web do
         pipe_through(:browser)
         pipe_through(:admin_required)
 
-        # live "/boundaries/instance/", Boundaries, as: :admin_settings
+        # live "/instance/", Boundaries, as: :admin_settings
       end
     end
   end
