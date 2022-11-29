ae7a64a794296280b5286eb3a06e0e0e3f7e350b
Mayel de Borniol <git@mayel.space>
2022-11-29 14:27:42 +1300

    Function as multi repo umbrella

--- mix.exs
+++ mix.exs
@@ -1,9 +1,20 @@
-Code.eval_file("mess.exs")
+Code.eval_file("mess.exs", (if File.exists?("../../lib/mix/mess.exs"), do: "../../lib/mix/"))
 
 defmodule Bonfire.Boundaries.MixProject do
   use Mix.Project
 
   def project do
+    if File.exists?("../../.is_umbrella.exs") do
+      [
+        build_path: "../../_build",
+        config_path: "../../config/config.exs",
+        deps_path: "../../deps",
+        lockfile: "../../mix.lock"
+      ]
+    else
+      []
+    end
+    ++
     [
       app: :bonfire_boundaries,
       version: "0.1.0",
@@ -15,7 +26,7 @@ defmodule Bonfire.Boundaries.MixProject do
       deps:
         Mess.deps([
           {:phoenix_live_reload, "~> 1.2", only: :dev},
-          {:dbg, "~> 1.0", only: [:dev, :test]},
+
           {:floki, ">= 0.0.0", only: [:dev, :test]}
         ])
     ]
