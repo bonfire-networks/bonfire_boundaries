9d503ee52d5ea1038fdfd0285febc8044d9958c8
ivanminutillo <ivanminutillo@outlook.it>
2023-01-11 17:03:42 +0100

    ui

--- lib/web/components/circles/header_circle/header_circle_live.ex
+++ lib/web/components/circles/header_circle/header_circle_live.ex
@@ -1,9 +1,10 @@
 defmodule Bonfire.Boundaries.Web.HeaderCircleLive do
   use Bonfire.UI.Common.Web, :stateless_component
-
+  
   prop circle, :map, required: true
   prop stereotype_id, :string, default: nil
   prop read_only, :boolean, default: false
   prop suggestions, :list, default: []
   prop myself, :map, default: nil
+
 end
--- lib/web/components/sidebar/sidebar_mobile_live.ex
+++
@@ -1,6 +0,0 @@
-defmodule Bonfire.Boundaries.Web.SidebarMobileLive do
-  use Bonfire.UI.Common.Web, :stateless_component
-
-  prop selected_tab, :string
-  prop id, :string, default: nil
-end
