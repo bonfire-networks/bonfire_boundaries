863197241f54808d95e8562730d323e63c11b93c
Mayel de Borniol <git@mayel.space>
2023-02-03 11:37:52 +1300

    Modal Form

--- extensions/bonfire/lib/web/icons/heroicons-outline/information-circle.ex
+++
@@ -1,8 +0,0 @@
-defmodule Iconify.HeroiconsOutline.InformationCircle do
-  use Phoenix.Component
-  def render(assigns) do
-    ~H"""
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 1 1-18 0a9 9 0 0 1 18 0Z"></path></svg>
-    """
-  end
-end
