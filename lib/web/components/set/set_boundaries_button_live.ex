b1c35d116dcf646d71a82920ad3686a4ac499cb2
ivanminutillo <ivanminutillo@outlook.it>
2023-03-17 18:07:27 +0100

    composer

--- lib/web/components/set/set_boundaries_button_live.ex
+++ lib/web/components/set/set_boundaries_button_live.ex
@@ -2,5 +2,6 @@ defmodule Bonfire.Boundaries.Web.SetBoundariesButtonLive do
   use Bonfire.UI.Common.Web, :stateless_component
   use Bonfire.Common.Utils
 
+  prop to_boundaries, :any, default: nil
   prop preset_boundary, :any, default: nil
 end
