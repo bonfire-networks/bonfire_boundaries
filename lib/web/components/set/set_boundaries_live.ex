35cad8fe59c10dca5853dc94c720777eaa268c6c
ivanminutillo <ivanminutillo@outlook.it>
2023-03-21 18:05:06 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/474

--- lib/web/components/set/set_boundaries_live.ex
+++ lib/web/components/set/set_boundaries_live.ex
@@ -52,11 +52,19 @@ defmodule Bonfire.Boundaries.Web.SetBoundariesLive do
     |> Enum.map(fn
       %Bonfire.Data.AccessControl.Acl{} = acl ->
         {e(acl, :named, :name, nil) || e(acl, :sterotyped, :named, :name, nil),
-         %{id: e(acl, :id, nil), field: :to_boundaries}}
+         %{
+            id: e(acl, :id, nil),
+            field: :to_boundaries,
+            name: e(acl, :named, :name, nil) || e(acl, :sterotyped, :named, :name, nil)
+          }}
 
       %Bonfire.Data.AccessControl.Circle{} = circle ->
         {e(circle, :named, :name, nil) || e(circle, :sterotyped, :named, :name, nil),
-         %{id: e(circle, :id, nil), field: :to_circles}}
+         %{
+            id: e(circle, :id, nil),
+            field: :to_circles,
+            name: e(circle, :named, :name, nil) || e(circle, :sterotyped, :named, :name, nil)
+          }}
 
       user ->
         name = e(user, :profile, :name, nil)
@@ -75,12 +83,16 @@ defmodule Bonfire.Boundaries.Web.SetBoundariesLive do
     |> debug()
   end
 
+  def list_my_boundaries(socket) do
+    current_user = current_user(socket)
+    Bonfire.Boundaries.Acls.list_my(current_user)
+  end
+
   def do_handle_event("live_select_change", %{"id" => live_select_id, "text" => search}, socket) do
     current_user = current_user(socket)
-
-    (Bonfire.Boundaries.Acls.list_my(current_user, search: search) ++
-       Bonfire.Boundaries.Circles.list_my(current_user, search: search) ++
-       Bonfire.Me.Users.search(search))
+    # Bonfire.Boundaries.Acls.list_my(current_user, search: search) ++
+    (Bonfire.Boundaries.Circles.list_my(current_user, search: search) ++
+      Bonfire.Me.Users.search(search))
     |> results_for_multiselect()
     |> maybe_send_update(LiveSelect.Component, live_select_id, options: ...)
 
