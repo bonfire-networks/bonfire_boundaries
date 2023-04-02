7f6d3b2529fabf671883fdfe8186acff36ef9c6f
Mayel de Borniol <git@mayel.space>
2023-04-03 11:44:39 +1200

    c

--- lib/queries.ex
+++ lib/queries.ex
@@ -227,7 +227,7 @@ defmodule Bonfire.Boundaries.Queries do
 
     (Common.Config.env() != :prod and
        Common.Config.get(:skip_all_boundary_checks)) ||
-      (is_list(opts) and Keyword.get(opts, :skip_boundary_check)) ||
+      (is_list(opts) and Keyword.get(opts, :skip_boundary_check) == true) ||
       (not is_nil(object) and Common.Enums.id(object) == Common.Enums.id(agent))
   end
 
