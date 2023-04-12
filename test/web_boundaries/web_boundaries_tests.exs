a13fce56613b945a552681e9ed315308b1b513d1
Mayel de Borniol <git@mayel.space>
2023-04-12 14:17:03 +1200

    f

--- test/web_boundaries/web_boundaries_tests.exs
+++ test/web_boundaries/web_boundaries_tests.exs
@@ -6,7 +6,6 @@ defmodule Bonfire.Boundaries.WebBoundariesTests do
   alias Bonfire.Social.FeedActivities
   alias Bonfire.Boundaries
 
-
   test "creating a post with a 'public' boundary and verify that all users can see and interact with it" do
     account = fake_account!()
     # Given a user
@@ -26,52 +25,41 @@ defmodule Bonfire.Boundaries.WebBoundariesTests do
         boundary: "public"
       }
     }
+
     assert {:ok, post} = Posts.publish(current_user: bob, post_attrs: attrs)
     assert post.post_content.name =~ "name"
   end
 
   test "creating a post with a 'local' boundary and verify that only users from that instance can see and interact with it." do
-
   end
 
   test "creating a post with a 'mention' boundary and verify that only mentioned users can see and interact with it." do
-
   end
 
   test "Test creating a post with a 'custom' boundary and verify that only specified users or circles can see and interact with it according to their assigned roles." do
-
   end
 
   test "Test adding a user with a 'see' role and verify that the user can see the post but not interact with it." do
-
   end
 
   test "adding a user with a 'read' role and verify that the user can read the post's content but not interact with it." do
-
   end
 
   test "adding a user with an 'interact' role and verify that the user can like and boost the post." do
-
   end
 
   test "adding a user with a 'participate' role and verify that the user can engage in the post's activities and discussions." do
-
   end
 
   test "adding a user with a 'caretaker' role and verify that the user can delete the post" do
-
   end
 
   test "adding a user with a 'none' role and verify that the user cannot see or interact with the post in any way." do
-
   end
 
   test "creating a post with a circle, and verify that only users within the circle can access the post according to their assigned roles." do
-
   end
 
   test "creating a post with a custom boundary, and verify that only users within the boundary can access the post according to their assigned roles." do
-
   end
-
 end
