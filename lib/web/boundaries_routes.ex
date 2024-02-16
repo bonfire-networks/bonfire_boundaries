c17f7b9c24aeadfa179c18776bb5e0497895eacb
Mayel de Borniol <git@mayel.space>
2024-02-16 13:24:46 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/851

--- lib/web/boundaries_routes.ex
+++ lib/web/boundaries_routes.ex
@@ -8,45 +8,42 @@ defmodule Bonfire.Boundaries.Web.Routes do
         pipe_through(:browser)
       end
 
-      # pages only guests can view
-      scope "/boundaries", Bonfire.Boundaries.Web do
-        pipe_through(:browser)
-        pipe_through(:guest_only)
-      end
+      if extension_enabled?(:bonfire_ui_me) do
+        # pages only guests can view
+        scope "/boundaries", Bonfire.Boundaries.Web do
+          pipe_through(:browser)
+          pipe_through(:guest_only)
+        end
 
-      scope "/boundaries", Bonfire do
-        pipe_through(:browser)
-        pipe_through(:account_required)
-      end
+        # pages you need an account to view
+        scope "/boundaries", Bonfire.Boundaries.Web do
+          pipe_through(:browser)
+          pipe_through(:account_required)
+        end
 
-      # pages you need an account to view
-      scope "/boundaries", Bonfire.Boundaries.Web do
-        pipe_through(:browser)
-        pipe_through(:account_required)
-      end
+        # pages you need to view as a user
+        scope "/boundaries", Bonfire.Boundaries.Web do
+          pipe_through(:browser)
+          pipe_through(:user_required)
 
-      # pages you need to view as a user
-      scope "/boundaries", Bonfire.Boundaries.Web do
-        pipe_through(:browser)
-        pipe_through(:user_required)
+          live("/scope/:scope", BoundariesLive, as: :boundaries)
+          live("/scope/:scope/:tab", BoundariesLive, as: :boundaries)
+          live("/scope/:scope/:tab/:id", BoundariesLive, as: :boundaries)
+          live("/scope/:scope/:tab/:id/:section", BoundariesLive, as: :boundaries)
 
-        live("/scope/:scope", BoundariesLive, as: :boundaries)
-        live("/scope/:scope/:tab", BoundariesLive, as: :boundaries)
-        live("/scope/:scope/:tab/:id", BoundariesLive, as: :boundaries)
-        live("/scope/:scope/:tab/:id/:section", BoundariesLive, as: :boundaries)
+          live("/", BoundariesLive, as: :boundaries)
+          live("/:tab", BoundariesLive, as: :boundaries)
+          live("/:tab/:id", BoundariesLive, as: :boundaries)
+          live("/:tab/:id/:section", BoundariesLive, as: :boundaries)
+        end
 
-        live("/", BoundariesLive, as: :boundaries)
-        live("/:tab", BoundariesLive, as: :boundaries)
-        live("/:tab/:id", BoundariesLive, as: :boundaries)
-        live("/:tab/:id/:section", BoundariesLive, as: :boundaries)
-      end
+        # pages only admins can view
+        scope "/boundaries", Bonfire.Boundaries.Web do
+          pipe_through(:browser)
+          pipe_through(:admin_required)
 
-      # pages only admins can view
-      scope "/boundaries", Bonfire.Boundaries.Web do
-        pipe_through(:browser)
-        pipe_through(:admin_required)
-
-        # live "/instance/", Boundaries, as: :admin_settings
+          # live "/instance/", Boundaries, as: :admin_settings
+        end
       end
     end
   end
