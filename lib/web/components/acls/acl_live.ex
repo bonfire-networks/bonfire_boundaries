f81dba5585df861086bdde1de8d9e8eb57151441
Mayel de Borniol <git@mayel.space>
2024-08-10 10:36:11 +0100

    f

--- lib/web/components/acls/acl_live.ex
+++ lib/web/components/acls/acl_live.ex
@@ -254,7 +254,6 @@ defmodule Bonfire.Boundaries.Web.AclLive do
      )}
   end
 
-
   def handle_event(
         "live_select_change",
         %{"id" => live_select_id, "text" => search},
@@ -274,8 +273,6 @@ defmodule Bonfire.Boundaries.Web.AclLive do
        ))
     |> results_for_multiselect(live_select_id, socket)
   end
-
-
 
   def handle_event(
         "live_select_change",
@@ -304,6 +301,7 @@ defmodule Bonfire.Boundaries.Web.AclLive do
         socket
       ) do
     current_user = current_user(socket.assigns)
+
     (Bonfire.Boundaries.Circles.list_my_with_global(
        [current_user, Bonfire.Boundaries.Fixtures.activity_pub_circle()],
        search: search
