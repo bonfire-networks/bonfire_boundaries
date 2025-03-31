7b0e2c9f3d1758fa54c29a7032e224c9b4ec88f7
Mayel de Borniol <git@mayel.space>
2025-03-31 11:10:14 +0100

    fix

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
-{:ok, _} = Bonfire.Common.Repo.start_link()
+Bonfire.Common.Repo.start_link()
 
 ExUnit.start(exclude: Bonfire.Common.RuntimeConfig.skip_test_tags())
 Ecto.Adapters.SQL.Sandbox.mode(Bonfire.Common.Config.repo(), :manual)
