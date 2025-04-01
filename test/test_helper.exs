87b6c12a1e127859ce8f3829152949e91d81e3b7
ivan <ivanminutillo@outlook.it>
2025-04-01 09:25:21 +0200

    tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,5 @@
 Bonfire.Common.Repo.start_link()
+# {:ok, _} = Bonfire.Common.Repo.start_link()
 
 ExUnit.start(exclude: Bonfire.Common.RuntimeConfig.skip_test_tags())
 Ecto.Adapters.SQL.Sandbox.mode(Bonfire.Common.Config.repo(), :manual)
